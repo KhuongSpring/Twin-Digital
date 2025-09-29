b352e2f0a4575add036d3b2815152c212f901901
khuong1742005 <boyzsno1@gmail.com>
2025-09-29 13:37:01 +0700

    feat(dynamic-spec-service, digital-aggregator-service): config kafka to project (issue #2)

---
+++ twin-digital/digital-aggregator-service/src/main/java/com/example/digital_aggregator_service/DigitalAggregatorServiceApplication.java
@@ -0,0 +1,29 @@
+package com.example.digital_aggregator_service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+@Log4j2
+@RequiredArgsConstructor
+@SpringBootApplication(scanBasePackages = "com.example.digital_aggregator_service")
+public class DigitalAggregatorServiceApplication {
+
+	public static void main(String[] args) {
+		Environment env = SpringApplication.run(DigitalAggregatorServiceApplication.class, args).getEnvironment();
+		String appName = env.getProperty("spring.application.name");
+		if (appName != null) {
+			appName = appName.toUpperCase();
+		}
+		String port = env.getProperty("server.port");
+		log.info("-------------------------START " + appName
+				+ " Application------------------------------");
+		log.info("   Application         : " + appName);
+		log.info("   Url swagger-ui      : http://localhost:" + port + "/swagger-ui.html");
+		log.info("-------------------------START SUCCESS " + appName
+				+ " Application------------------------------");
+	}
+
+}
